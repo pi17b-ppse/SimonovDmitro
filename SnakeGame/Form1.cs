dd6d301b2b419a9b20aee4eb7707899f8bf46b8b
Simon <despite8death@gmail.com>
2021-03-02 10:56:51 +0300

    commit3

---
+++ SnakeGame/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private List<Circle> Snake = new List<Circle>();  //создание списка circle для  змейки
+        private Circle food = new Circle(); // создание подкласса circle еда
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void updateGraphics(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
