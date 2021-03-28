41a15bb32a066a0c330b925861e5b6b2a1541d7c
Simonov <despite8death@gmail.com>
2021-03-29 00:25:27 +0300

    changed Settings

--- WindowsFormsApp1/WindowsFormsApp1/Settings.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Settings.cs
@@ -14,24 +14,22 @@ namespace WindowsFormsApp1
         Left, Right, Up, Down
     };
 
-    /// <summary>
-    /// Класс отвечает за все действия на форме.
-    /// </summary>
+ 
     class Settings
     {
-        /// <value>Gets and sets the value of Height.</value>
+        
         public static int Height { get; set; }
-        /// <value>Gets  and sets the value of Widht.</value>
+        
         public static int Widht { get; set; }
-        /// <value>Gets and sets the value of Speed.</value>
+        
         public static int Speed { get; set; }
-        /// <value>Gets and sets the value of Point.</value>
+        
         public static int Point { get; set; }
-        /// <value>Gets and sets the value of Score.</value>
+        
         public static int Score { get; set; }
-        /// <value>Gets and sets the value of EndGame.</value>
+        
         public static bool EndGame { get; set; }
-        /// <value>Gets and sets the value of direction.</value>
+        
         public static Directions direction { get; set; }
 
 
