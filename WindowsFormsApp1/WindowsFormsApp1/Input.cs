26523f4f7112d898e8c111bbdeb30d1cd54b2e8d
Simonov <despite8death@gmail.com>
2021-03-28 23:05:59 +0300

    added KeyPress func

--- WindowsFormsApp1/WindowsFormsApp1/Input.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Input.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Input
+    {
+        public static Hashtable keyTable = new Hashtable();
+
+        public static bool KeyPress(Keys key)
+        {
+            if (keyTable[key] == null)
+            {
+                return false;
+            }
+
+            return (bool)keyTable[key];
+        }
+    }
+}
