9cc224c29b545972da9f1dfcd7b4c8246e217ed9
Simonov <despite8death@gmail.com>
2021-03-28 22:50:25 +0300

    modified Settings.cs

--- WindowsFormsApp1/WindowsFormsApp1/Settings.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Settings.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    public enum Directions
+    {        //Объявление направлений движения змейки
+        Left, Right, Up, Down
+    };
+}
